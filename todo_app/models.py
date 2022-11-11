27236c037ffffe2d93078bb7588c774c65767019
Krzysiek <xysiek99@gmail.com>
2022-11-11 13:07:32 +0100

    Basic app created with usage of 'simplecss.org' CSS framework, CRUD operations, models and views

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+
+def week_ahead():
+    return timezone.now() + timezone.timedelta(days=7)
+
+class ToDoList(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def get_absolute_url(self):
+        return reverse("list", args=[self.id])
+
+    # below is standard Python way of creating a readable object's representation
+    # not necessary to write this function, but helpful with debugging
+    def __str__(self):
+        return self.title
+
+class ToDoItem(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_due = models.DateTimeField(default=week_ahead)
+    todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+
+    # Then also add field called "creator"
+
+    def get_absolute_url(self):
+        return reverse(
+            "item-update", args=[str(self.todo_list.id), str(self.id)]
+        )
+
+    # below is standard Python way of creating a readable object's representation
+    # not necessary to write this function, but helpful with debugging
+    def __str__(self):
+        return f"{self.title}: due {self.time_due}"
+
+    class Meta:
+        ordering = ["time_due"]
